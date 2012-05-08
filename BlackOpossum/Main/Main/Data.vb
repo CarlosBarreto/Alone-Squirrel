14873d12250b6fa56f3ea4ab8d6d9dbb548cb2c1
Carlos Barreto <carlos.barreto@live.com.mx>
2012-05-08 18:12:47 -0500

    Trabajo en oficina

---
+++ BlackOpossum/Main/Main/Data.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Data
+Imports DataSource
+
+Namespace AppMain
+    Public Class ApplicationData
+        Private _DataSource As DBDataSource
+        Private _DataTable As DataTable
+        Private _strSQL As String
+
+        ''' <summary>
+        ''' Constructor
+        ''' </summary>
+        ''' <remarks></remarks>
+        Public Sub New()
+            _DataSource = New DBDataSource
+        End Sub
+
+        ''' <summary>
+        ''' Funcion que lee de la Base de Datos, el menú de opciones
+        ''' </summary>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Public Function ReadMenu() As DataTable
+            Dim DT As DataTable
+            DT = _DataSource.getDataTableQuery("Select * from sys_optionmenu;")
+            Return DT
+        End Function
+    End Class
+End Namespace
