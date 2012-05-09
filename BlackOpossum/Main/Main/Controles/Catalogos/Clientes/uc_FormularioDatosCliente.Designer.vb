43a8080e43ff92001bc90d8c0370188d2e750ada
Carlos Barreto <carlos.barreto@live.com.mx>
2012-05-09 18:21:22 -0500

    Trabajo en oficina

---
+++ BlackOpossum/Main/Main/Controles/Catalogos/Clientes/uc_FormularioDatosCliente.Designer.vb
@@ -0,0 +1,330 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class uc_FormularioDatosCliente
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txt_NumeroCliente = New System.Windows.Forms.TextBox()
+        Me.txt_Ramo = New System.Windows.Forms.TextBox()
+        Me.txt_Telefono = New System.Windows.Forms.TextBox()
+        Me.txt_Estado = New System.Windows.Forms.TextBox()
+        Me.txt_Ciudad = New System.Windows.Forms.TextBox()
+        Me.txt_CP = New System.Windows.Forms.TextBox()
+        Me.txt_Colonia = New System.Windows.Forms.TextBox()
+        Me.txt_Domicilio = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txt_Empresa = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.bt_Cancelar = New System.Windows.Forms.Button()
+        Me.bt_Guardar = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer))
+        Me.Label1.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Label1.Location = New System.Drawing.Point(33, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(113, 18)
+        Me.Label1.TabIndex = 21
+        Me.Label1.Text = "Número de Cliente"
+        Me.Label1.Visible = False
+        '
+        'txt_NumeroCliente
+        '
+        Me.txt_NumeroCliente.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txt_NumeroCliente.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_NumeroCliente.ForeColor = System.Drawing.Color.Black
+        Me.txt_NumeroCliente.Location = New System.Drawing.Point(152, 64)
+        Me.txt_NumeroCliente.Name = "txt_NumeroCliente"
+        Me.txt_NumeroCliente.Size = New System.Drawing.Size(132, 22)
+        Me.txt_NumeroCliente.TabIndex = 38
+        Me.txt_NumeroCliente.Visible = False
+        '
+        'txt_Ramo
+        '
+        Me.txt_Ramo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txt_Ramo.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_Ramo.ForeColor = System.Drawing.Color.Black
+        Me.txt_Ramo.Location = New System.Drawing.Point(152, 274)
+        Me.txt_Ramo.Name = "txt_Ramo"
+        Me.txt_Ramo.Size = New System.Drawing.Size(348, 22)
+        Me.txt_Ramo.TabIndex = 37
+        '
+        'txt_Telefono
+        '
+        Me.txt_Telefono.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txt_Telefono.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_Telefono.ForeColor = System.Drawing.Color.Black
+        Me.txt_Telefono.Location = New System.Drawing.Point(152, 244)
+        Me.txt_Telefono.Name = "txt_Telefono"
+        Me.txt_Telefono.Size = New System.Drawing.Size(348, 22)
+        Me.txt_Telefono.TabIndex = 36
+        '
+        'txt_Estado
+        '
+        Me.txt_Estado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txt_Estado.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_Estado.ForeColor = System.Drawing.Color.Black
+        Me.txt_Estado.Location = New System.Drawing.Point(152, 214)
+        Me.txt_Estado.Name = "txt_Estado"
+        Me.txt_Estado.Size = New System.Drawing.Size(348, 22)
+        Me.txt_Estado.TabIndex = 35
+        '
+        'txt_Ciudad
+        '
+        Me.txt_Ciudad.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txt_Ciudad.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_Ciudad.ForeColor = System.Drawing.Color.Black
+        Me.txt_Ciudad.Location = New System.Drawing.Point(152, 184)
+        Me.txt_Ciudad.Name = "txt_Ciudad"
+        Me.txt_Ciudad.Size = New System.Drawing.Size(348, 22)
+        Me.txt_Ciudad.TabIndex = 34
+        '
+        'txt_CP
+        '
+        Me.txt_CP.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txt_CP.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_CP.ForeColor = System.Drawing.Color.Black
+        Me.txt_CP.Location = New System.Drawing.Point(409, 154)
+        Me.txt_CP.Name = "txt_CP"
+        Me.txt_CP.Size = New System.Drawing.Size(91, 22)
+        Me.txt_CP.TabIndex = 33
+        '
+        'txt_Colonia
+        '
+        Me.txt_Colonia.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txt_Colonia.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_Colonia.ForeColor = System.Drawing.Color.Black
+        Me.txt_Colonia.Location = New System.Drawing.Point(152, 154)
+        Me.txt_Colonia.Name = "txt_Colonia"
+        Me.txt_Colonia.Size = New System.Drawing.Size(200, 22)
+        Me.txt_Colonia.TabIndex = 32
+        '
+        'txt_Domicilio
+        '
+        Me.txt_Domicilio.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txt_Domicilio.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_Domicilio.ForeColor = System.Drawing.Color.Black
+        Me.txt_Domicilio.Location = New System.Drawing.Point(152, 124)
+        Me.txt_Domicilio.Name = "txt_Domicilio"
+        Me.txt_Domicilio.Size = New System.Drawing.Size(348, 22)
+        Me.txt_Domicilio.TabIndex = 31
+        '
+        'Label7
+        '
+        Me.Label7.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer))
+        Me.Label7.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Label7.Location = New System.Drawing.Point(33, 246)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(66, 18)
+        Me.Label7.TabIndex = 27
+        Me.Label7.Text = "Teléfono"
+        '
+        'Label6
+        '
+        Me.Label6.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer))
+        Me.Label6.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Label6.Location = New System.Drawing.Point(33, 216)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(58, 18)
+        Me.Label6.TabIndex = 26
+        Me.Label6.Text = "Estado"
+        '
+        'Label5
+        '
+        Me.Label5.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer))
+        Me.Label5.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Label5.Location = New System.Drawing.Point(33, 186)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(59, 18)
+        Me.Label5.TabIndex = 25
+        Me.Label5.Text = "Ciudad"
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer))
+        Me.Label4.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Label4.Location = New System.Drawing.Point(33, 156)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(62, 18)
+        Me.Label4.TabIndex = 24
+        Me.Label4.Text = "Colonia"
+        '
+        'txt_Empresa
+        '
+        Me.txt_Empresa.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txt_Empresa.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_Empresa.ForeColor = System.Drawing.Color.Black
+        Me.txt_Empresa.Location = New System.Drawing.Point(152, 94)
+        Me.txt_Empresa.Name = "txt_Empresa"
+        Me.txt_Empresa.Size = New System.Drawing.Size(348, 22)
+        Me.txt_Empresa.TabIndex = 30
+        '
+        'Label9
+        '
+        Me.Label9.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer))
+        Me.Label9.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Label9.Location = New System.Drawing.Point(367, 156)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(31, 18)
+        Me.Label9.TabIndex = 29
+        Me.Label9.Text = "CP"
+        '
+        'Label8
+        '
+        Me.Label8.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer))
+        Me.Label8.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Label8.Location = New System.Drawing.Point(33, 276)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(50, 18)
+        Me.Label8.TabIndex = 28
+        Me.Label8.Text = "Ramo"
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer))
+        Me.Label3.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Label3.Location = New System.Drawing.Point(33, 126)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(74, 18)
+        Me.Label3.TabIndex = 23
+        Me.Label3.Text = "Domicilio"
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer))
+        Me.Label2.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Label2.Location = New System.Drawing.Point(33, 96)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 18)
+        Me.Label2.TabIndex = 22
+        Me.Label2.Text = "Empresa"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.bt_Cancelar)
+        Me.Panel1.Controls.Add(Me.bt_Guardar)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(513, 34)
+        Me.Panel1.TabIndex = 41
+        '
+        'bt_Cancelar
+        '
+        Me.bt_Cancelar.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bt_Cancelar.ForeColor = System.Drawing.Color.Black
+        Me.bt_Cancelar.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.bt_Cancelar.Location = New System.Drawing.Point(103, 3)
+        Me.bt_Cancelar.Name = "bt_Cancelar"
+        Me.bt_Cancelar.Size = New System.Drawing.Size(89, 22)
+        Me.bt_Cancelar.TabIndex = 42
+        Me.bt_Cancelar.Text = "&Cancelar"
+        Me.bt_Cancelar.UseVisualStyleBackColor = True
+        '
+        'bt_Guardar
+        '
+        Me.bt_Guardar.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bt_Guardar.ForeColor = System.Drawing.Color.Black
+        Me.bt_Guardar.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.bt_Guardar.Location = New System.Drawing.Point(8, 3)
+        Me.bt_Guardar.Name = "bt_Guardar"
+        Me.bt_Guardar.Size = New System.Drawing.Size(89, 22)
+        Me.bt_Guardar.TabIndex = 41
+        Me.bt_Guardar.Text = "&Guardar"
+        Me.bt_Guardar.UseVisualStyleBackColor = True
+        '
+        'uc_FormularioDatosCliente
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Transparent
+        Me.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt_NumeroCliente)
+        Me.Controls.Add(Me.txt_Ramo)
+        Me.Controls.Add(Me.txt_Telefono)
+        Me.Controls.Add(Me.txt_Estado)
+        Me.Controls.Add(Me.txt_Ciudad)
+        Me.Controls.Add(Me.txt_CP)
+        Me.Controls.Add(Me.txt_Colonia)
+        Me.Controls.Add(Me.txt_Domicilio)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txt_Empresa)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Name = "uc_FormularioDatosCliente"
+        Me.Size = New System.Drawing.Size(513, 508)
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txt_NumeroCliente As System.Windows.Forms.TextBox
+    Friend WithEvents txt_Ramo As System.Windows.Forms.TextBox
+    Friend WithEvents txt_Telefono As System.Windows.Forms.TextBox
+    Friend WithEvents txt_Estado As System.Windows.Forms.TextBox
+    Friend WithEvents txt_Ciudad As System.Windows.Forms.TextBox
+    Friend WithEvents txt_CP As System.Windows.Forms.TextBox
+    Friend WithEvents txt_Colonia As System.Windows.Forms.TextBox
+    Friend WithEvents txt_Domicilio As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txt_Empresa As System.Windows.Forms.TextBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents bt_Cancelar As System.Windows.Forms.Button
+    Friend WithEvents bt_Guardar As System.Windows.Forms.Button
+
+End Class
---
+++ BlackOpossum/Main/Main/fr_Main.vb
@@ -0,0 +1,17 @@
+﻿Imports Main.AppMain
+
+Public Class fr_Main
+    Private
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Panel1.Width = Me.Width - PictureBox1.Width - 20
+
+        Dim AD As New ApplicationData
+        Dim DT As DataTable
+        DT = AD.ReadMenu
+
+
+        AD = Nothing
+        DT = Nothing
+    End Sub
+
+End Class
